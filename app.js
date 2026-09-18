acf37788f43baf98bf6adfe613f5f2580057e9c1
Seemon-ch <seemonchaudhary99@gmail.com>
2026-09-18 11:06:13 +0530

    created model listing

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const Listing = require('./models/listing.js');
 
 main().then(()=>{
     console.log("connected to database");
@@ -14,6 +15,20 @@ async function main(){
 
 app.get("/", (req,res)=>{
     res.send("WELCOME TO ROOT PAGE");
+});
+
+//testing schema
+app.get("/testListing" , async (req,res)=>{
+    let sampleListing = new Listing({
+        title:"My new Vila",
+        description:"near beach ",
+        price:1200,
+        location:"calangute,Goa",
+        country:"India"
+    });
+    await sampleListing.save();
+    console.log("saved sample");
+    res.send("sucessful");
 });
 
 app.listen("8080",()=>{
